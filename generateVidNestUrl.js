d95467c1986edefeabdd5f6efc0676c44d9858f0
titan <flameplayz937@gmail.com>
2025-08-28 15:57:17 +0100

    Create generateVidNestUrl.js

---
+++ generateVidNestUrl.js
@@ -0,0 +1,6 @@
+// generateVidNestUrl.js
+const generateVidNestUrl = (anilistId, episodeNumber) => {
+  return `https://vidnest.fun/anime/${anilistId}/${episodeNumber}/hindi`;
+};
+
+module.exports = generateVidNestUrl;
